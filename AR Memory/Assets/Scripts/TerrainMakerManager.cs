d0cc58c7ce5d3a84efc58b3a8e90487db9a0748d
DESKTOP-AV91D2I\paosa <paosalcedo@gmail.com>
2017-11-11 14:10:13 -0500

    Successfully got colors in

--- AR Memory/Assets/Scripts/TerrainMakerManager.cs
+++ AR Memory/Assets/Scripts/TerrainMakerManager.cs
@@ -16,7 +16,21 @@ public class TerrainMakerManager : MonoBehaviour {
 			{
 				perlinNoise = Mathf.PerlinNoise(i * refinement, j * refinement);
 				GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+				MeshRenderer cubeMesh = cube.GetComponent<MeshRenderer>();
+				if(perlinNoise >= 0.75f){
+					cubeMesh.material.color = Color.white;
+				}
+				else if(perlinNoise < 0.75f && perlinNoise >= 0.5f){
+					cubeMesh.material.color = Color.gray;
+				}
+				else if(perlinNoise < 0.5f && perlinNoise >= 0.25f){
+					cubeMesh.material.color = Color.green;
+				}
+				else if(perlinNoise < 0.25f && perlinNoise >= 0f){
+					cubeMesh.material.color = Color.cyan;
+				}
 				cube.transform.position = new Vector3(transform.position.x + i, transform.position.y + (perlinNoise * multiplier), transform.position.z + j);
+
 			}
 		}
 	}
